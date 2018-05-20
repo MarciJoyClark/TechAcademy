bb3ca33e93065b4d5d983670ab28cb18eacea4a8
Marci-Joy <MarciJoyClark@gmail.com>
2018-05-19 21:02:02 -0700

    Mathupload
    Math upload to GH

---
+++ Math/Math/Math_fnc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public class Math_fnc
+    {
+            public Math_fnc()
+            {          
+                try
+                {
+                    Console.WriteLine("Pick a positive whole number.");
+                    int numberOne = Convert.ToInt32(Console.ReadLine());
+                        int numberAdd = numberOne + 13;                
+                    Console.WriteLine("That number + 13 is " + numberAdd +  ". Pick a higher number!");
+                    int numberTwo = Convert.ToInt32(Console.ReadLine());
+                        int numberSub = numberTwo - numberOne;
+                    Console.WriteLine("Your new number minus the first one you gave me is " +numberSub+ "!");
+                    int numberThree = numberSub + numberAdd;
+                    Console.WriteLine(numberSub + " Plus " + numberAdd + " is " + numberThree + ". Pretty cool, huh? Now high five your computer for being so smart!");
+
+                    Console.ReadLine();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please enter an integer for me to use.");
+                    return;
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("Don't give me a \"0\", please!");
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("You messed something up! Try again!");
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }
+            }
+    }
+}
---
+++ Math/Math/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Math_fnc Math = new Math_fnc();
+            Console.Read();
+        }
+    }
+}
